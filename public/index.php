d36f92f561611def4b28ee04d43acb76ab1ee36b
Talu <talupark@Talulahs-MacBook-Pro.local>
2024-11-20 09:48:08 +0100

    commit talu

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }require_once '../config/db.php';
-
+//talu
 global $pdo;
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
